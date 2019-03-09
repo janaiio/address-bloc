1371b2a9722a159c37dc13fc748c68b10ddc74d1
unknown <janais1991@gmail.com>
2019-03-08 21:54:09 -0600

    Completed; no test errors

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -18,19 +18,19 @@ describe("ContactController", () => {
     });
   });
 
-});
-
-describe("#addContact()", () => {
-  it("should add a single contact into the book", (done) => {
-    this.book.addContact("Alice", "001-101-1010")
-    .then((contact) => {
-      expect(contact.name).toBe("Alice");
-      expect(contact.phone).toBe("001-101-1010");
-      done();
-    })
-    .catch((err) => {
-      done();
+  describe("#addContact()", () => {
+    it("should add a single contact into the book", (done) => {
+      this.book.addContact("Alice", "001-101-1010", "alice@email.com")
+      .then((contact) => {
+        expect(contact.name).toBe("Alice");
+        expect(contact.phone).toBe("001-101-1010");
+        expect(contact.email).toBe("alice@email.com");
+        done();
+      })
+      .catch((err) => {
+        done();
+      });
     });
-
   });
+
 });
