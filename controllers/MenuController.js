53b4837efb8692f45a419d3e605193f8b9c4f391
unknown <janais1991@gmail.com>
2019-03-08 21:19:53 -0600

    In Progress;  undefined in contact_controller_spec.js

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -17,7 +17,7 @@ module.exports = class MenuController {
             "Exit" //exit the program
           ]
       }
-    ];
+    ]
     this.book = new ContactController();
   }
 
@@ -54,7 +54,7 @@ module.exports = class MenuController {
   addContact(){
     this.clear();
     inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-      this.book.addContact(answers.name, answers.phone).then((contact) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
         console.log("Contact added successfully!");
         this.main();
       }).catch((err) => {
